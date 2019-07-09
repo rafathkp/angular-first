482285fa8cec491fcf0159b47d06c8fd828ac04f
rafath <rafath.raaz@gmail.com>
2019-07-09 12:51:12 +0530

    first anguar commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
